3e1ef6846983ffde960c0c9b3eb9297dc7b3eaa6
nator333 <m.nakamata35@gmail.com>
2019-08-19 19:24:18 -0700

    Remove phone number & user name

--- AWSAuthSDK/Sources/AWSUserPoolsSignIn/UserPoolsUI/AWSUserPoolForgotPasswordViewController.m
+++ AWSAuthSDK/Sources/AWSUserPoolsSignIn/UserPoolsUI/AWSUserPoolForgotPasswordViewController.m
@@ -60,7 +60,7 @@
 }
 
 - (void)setUp {
-    _userNameRow = [[AWSFormTableCell alloc] initWithPlaceHolder:@"User Name" type:InputTypeText];
+    _userNameRow = [[AWSFormTableCell alloc] initWithPlaceHolder:@"Email" type:InputTypeText];
     _tableDelegate = [AWSFormTableDelegate new];
     [self.tableDelegate addCell:self.userNameRow];
     self.tableView.delegate = self.tableDelegate;
@@ -95,8 +95,8 @@
 - (IBAction)onForgotPassword:(id)sender {
     NSString *userName = [self.tableDelegate getValueForCell:self.userNameRow forTableView:self.tableView];
     if ([userName isEqualToString:@""]) {
-        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Missing Username"
-                                                                                 message:@"Please enter a valid username."
+        UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Missing Email"
+                                                                                 message:@"Please enter a valid email."
                                                                           preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Ok" style:UIAlertActionStyleDefault handler:nil];
         [alertController addAction:ok];
